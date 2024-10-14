45c1a61ea931e1917f2f9301049a66a95c934b8c
Khe2707 <130121589+Khe2707@users.noreply.github.com>
2024-10-14 19:13:15 +0700

    update car

---
+++ Assets/bailm/Scripts/ZombieSpawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpam : MonoBehaviour
+{
+    [Header("ZombieSpawn Var")]
+    public GameObject zombiePrefab;
+    public Transform zombieSpawnPosition;
+    private float repeatCycle = 1f;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            InvokeRepeating("EnemySpawner", 1f, repeatCycle);
+            Destroy(gameObject, 10f);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+    void EnemySpawner()
+    {
+        Instantiate(zombiePrefab, zombieSpawnPosition.position, zombieSpawnPosition.rotation);
+    }
+}
