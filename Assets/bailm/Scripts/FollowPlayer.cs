ebd7e382886bcfeff8249111e206300614f936c1
dangnhps27654 <130121808+dangnhps27654@users.noreply.github.com>
2024-11-12 03:59:39 +0700

    Pause, giaodien, iconPlayer, chuyển màn,
    giao diện cảu màn hình chào, pause nút đẹp lắm mà chưa có sài được

---
+++ Assets/bailm/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 10, 0); 
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = player.position + offset;
+            transform.rotation = Quaternion.Euler(90, player.eulerAngles.y, 0);
+        }
+    }
+}
