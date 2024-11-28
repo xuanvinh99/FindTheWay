42efc0787f2bf996ab71e1badfabf2661a42319a
Khe2707 <130121589+Khe2707@users.noreply.github.com>
2024-11-28 18:56:00 +0700

    update

--- Assets/bailm/Scripts/SceneChanger.cs
+++ Assets/bailm/Scripts/SceneChanger.cs
@@ -22,6 +22,6 @@ public class ScaneChanger : MonoBehaviour
 
     private void OnVideoEnd(VideoPlayer vp)
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Menuu");
     }
 }
