8b27584437cca02533fa6f5ca18563f112d4fab3
dangnhps27654 <130121808+dangnhps27654@users.noreply.github.com>
2024-12-09 13:41:34 +0700

    dongho + amthanh enemy

---
+++ Assets/bailm/Scripts/DongHo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DongHo : MonoBehaviour
+{
+    public Text timerText; 
+    private float elapsedTime = 0f; 
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+
+        timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+}
