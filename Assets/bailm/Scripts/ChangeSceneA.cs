194526e81b902c905373c728f47c54946ea8dc34
dangnhps27654 <130121808+dangnhps27654@users.noreply.github.com>
2024-12-13 03:33:34 +0700

    update

---
+++ Assets/bailm/Scripts/ChangeSceneA.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneA : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        // Đảm bảo videoPlayer không phải null
+        if (videoPlayer != null)
+        {
+            // Đăng ký sự kiện khi video kết thúc
+            videoPlayer.loopPointReached += OnVideoEnd;
+        }
+        else
+        {
+            Debug.LogError("VideoPlayer không được gán trong VideoController.");
+        }
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Scene_B");
+    }
+}
